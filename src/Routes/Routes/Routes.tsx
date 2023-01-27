daaae16f467b7337d2d82b739b6c39ba273dd243
Sadid <sfhsadid@gmail.com>
2023-01-27 21:04:08 +0600

    my Instructor area done

--- src/Routes/Routes/Routes.tsx
+++ src/Routes/Routes/Routes.tsx
@@ -1,12 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import AdminDashboard from "../../Layout/AdminDashboard/AdminDashboard";
 import Main from "../../Layout/Main/Main";
+import About from "../../Pages/About/About";
 import AddStudent from "../../Pages/AdminDashboard/Students/AddStudent/AddStudent";
 import AllStudents from "../../Pages/AdminDashboard/Students/AllStudents/AllStudents";
 import AddTeachers from "../../Pages/AdminDashboard/Teachers/AddTeachers/AddTeachers";
 import AllTeachers from "../../Pages/AdminDashboard/Teachers/AllTeachers/AllTeachers";
 import ErrorPage from "../../Pages/ErrorPage/ErrorPage";
 import Home from "../../Pages/Home/Home";
+import AllInstructor from "../../Pages/Instructor/AllInstructor/AllInstructor";
+import TeacherDeatils from "../../Pages/Instructor/TeacherDetails/TeacherDeatils";
 import Login from "../../Shared/Login/Login";
 import SignUp from "../../Shared/SignUp/SignUp";
 
@@ -28,6 +31,28 @@ export const router = createBrowserRouter([
         path: "/signup",
         element: <SignUp></SignUp>,
       },
+      {
+        path: "/all-instrutor",
+        element: <AllInstructor></AllInstructor>,
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/all-instrutor",
+        element: <AllInstructor></AllInstructor>,
+      },
+      {
+        path: "/about",
+        element: <About></About>,
+      },
+      {
+        path: "/instrctorDetails/:id",
+        element: <TeacherDeatils />,
+        loader: ({ params }) =>
+          fetch(`http://localhost:5000/instructorDetails/${params.id}`),
+      },
     ],
   },
   {
@@ -36,21 +61,20 @@ export const router = createBrowserRouter([
     children: [
       {
         path: "/admin/add-students",
-        element: <AddStudent></AddStudent>
+        element: <AddStudent></AddStudent>,
       },
       {
         path: "/admin/all-students",
-        element: <AllStudents></AllStudents>
+        element: <AllStudents></AllStudents>,
       },
       {
         path: "/admin/add-teachers",
-        element: <AddTeachers></AddTeachers>
+        element: <AddTeachers></AddTeachers>,
       },
       {
         path: "/admin/all-teachers",
-        element: <AllTeachers></AllTeachers>
+        element: <AllTeachers></AllTeachers>,
       },
-    ]
-  }
+    ],
+  },
 ]);
-
